703ec02cbcd3bec3bfebf86566814ca5e6aa3fc6
MegaNoob-stack <kasrahabibbeygi@gmail.com>
2020-03-03 10:02:23 -0800

    update dashbord

--- backend/templates/zend/assets/js/login.js
+++ backend/templates/zend/assets/js/login.js
@@ -1,20 +1,18 @@
-function adminlogerrors(){
+function adminlogerrors() {
     var usernameError = document.forms['dashbord_login_form']['username_inp'].value;
     var passwordError = document.forms['dashbord_login_form']['password_inp'].value;
     var captchaError = document.forms['dashbord_login_form']['captcha_inp'].value;
     var errorField = document.getElementById('dashbord_login_error');
-    if( usernameError == ''){
+    if (usernameError == '') {
         errorField.style.display = "block";
         errorField.innerHTML = "لطفا نام کاربری را وارد کنید";
         return false;
-    }
-    else if( passwordError == ''){
+    } else if (passwordError == '') {
         errorField.style.display = "block";
         errorField.innerHTML = "لطفا رمز عبور خود را وارد کنید";
         return false;
 
-    }
-    else if( captchaError == ''){
+    } else if (captchaError == '') {
         errorField.style.display = "block";
         errorField.innerHTML = "لطفا کد امنیتی را وارد کنید";
         return false;
