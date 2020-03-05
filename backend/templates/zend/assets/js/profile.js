c885bfc75e28c7b4dba99a427c16516ebdcd790f
MegaNoob-stack <kasrahabibbeygi@gmail.com>
2020-03-05 15:48:24 -0800

    do not anny thing

--- backend/templates/zend/assets/js/profile.js
+++ backend/templates/zend/assets/js/profile.js
@@ -1,17 +1,3 @@
-// var new_password = document.forms['admin_pro_pass_edit']['new_password'].value;
-// var confirm_password = document.forms['admin_pro_pass_edit']['confirm_password'].value;
-
-// var password_error = document.getElementById('admin_profile_password_error');
-
-// function passError(){
-//   if( new_password == confirm_password ){
-//     password_error.style.display = "block";
-//     password_error.innerHTML = "رمز عبور با تکرار رمز عبور یکسان نمی باشد";
-//     alert(new_password)
-//     return false;
-//   }
-// }
-
 var first_pro_box = document.getElementById('first_pro_box');
 var second_pro_box = document.getElementById('second_pro_box');
 
