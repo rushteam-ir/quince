c236e7ceca970ebf58d183df3de427c55f2e28be
MegaNoob-stack <kasrahabibbeygi@gmail.com>
2020-03-05 07:45:02 -0800

    update profile front-end

---
+++ backend/templates/zend/assets/js/profile.js
@@ -0,0 +1,31 @@
+new Vue({
+    el: '#profileCheckbox',
+    vuetify: new Vuetify(),
+    data () {
+      return {
+        proCheckBox: null,
+      }
+    },
+  })
+
+var first_pro_box = document.getElementById('first_pro_box');
+var second_pro_box = document.getElementById('second_pro_box');
+
+var admin_pro_edit = document.getElementById('admin_pro_edit');
+var admin_change_pass = document.getElementById('admin_change_pass');
+
+function secondChangeDisplay(){
+    admin_change_pass.style.display = "block";
+    admin_pro_edit.style.display = "none";
+    second_pro_box.style.backgroundColor = "white";
+    first_pro_box.style.backgroundColor = "#F7F8FB";
+
+}
+
+function firstChangeDisplay(){
+    admin_pro_edit.style.display = "block"
+    admin_change_pass.style.display = "none";
+    second_pro_box.style.backgroundColor = "#F7F8FB";
+    first_pro_box.style.backgroundColor = "white";
+
+}
