dd3145730cacf6903d90e49de08bc8f8e9051f4b
MegaNoob-stack <kasrahabibbeygi@gmail.com>
2020-03-08 05:32:22 -0700

    fix pro error

--- backend/templates/zend/assets/js/profile.js
+++ backend/templates/zend/assets/js/profile.js
@@ -4,24 +4,36 @@ var second_pro_box = document.getElementById('second_pro_box');
 var admin_pro_edit = document.getElementById('admin_pro_edit');
 var admin_change_pass = document.getElementById('admin_change_pass');
 
-function secondChangeDisplay(){
-    admin_change_pass.style.display = "block";
-    admin_pro_edit.style.display = "none";
-    second_pro_box.style.backgroundColor = "white";
-    first_pro_box.style.backgroundColor = "#F7F8FB";
-}
 
 function firstChangeDisplay(){
+
     admin_pro_edit.style.display = "block"
     admin_change_pass.style.display = "none";
     second_pro_box.style.backgroundColor = "#F7F8FB";
     first_pro_box.style.backgroundColor = "white";
 }
- 
+
+function secondChangeDisplay(){
+
+    admin_change_pass.style.display = "block";
+    admin_pro_edit.style.display = "none";
+    second_pro_box.style.backgroundColor = "white";
+    first_pro_box.style.backgroundColor = "#F7F8FB";  
+}
+
+function passerror(){
+    
+    var admin_profile_error_field_pass = document.getElementById('admin_profile_error_field_pass');
+    var current_password = document.forms['admin_pro_pass_edit']['current_password'].value;
+    
+    if (current_password == ''){
+        admin_profile_error_field_pass.style.display="block";
+        admin_profile_error_field_pass.innerHTML = "salam"
+        return false;
+    }
 
 
-
-
+}
 
 
 
