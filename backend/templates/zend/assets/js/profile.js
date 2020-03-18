12ec53d84d07025bd55b7a14e1b122ebba773921
MegaNoob-stack <kasrahabibbeygi@gmail.com>
2020-03-18 06:52:47 -0700

    شسیشسیشسی

--- backend/templates/zend/assets/js/profile.js
+++ backend/templates/zend/assets/js/profile.js
@@ -140,3 +140,22 @@ function deleteCall(){
 function redirect(url) {
     location.href = url
 }
+
+
+// chose file customize
+(function () {
+
+    $('.input-file-custom').each(function () {
+      var $input = $(this),
+        $label = $input.next('.js-labelFile'),
+        labelVal = $label.html();
+
+      $input.on('change', function (element) {
+        var fileName = '';
+        if (element.target.value) fileName = element.target.value.split('\\').pop();
+        fileName ? $label.addClass('has-file').find('.js-fileName').html(fileName) : $label.removeClass(
+          'has-file').html(labelVal);
+      });
+    });
+
+  })();
