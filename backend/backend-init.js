38651ae6a39f6e038e7c3026e82da326c975dcc6
rouzbehsbz <polybius1999@gmail.com>
2020-03-18 11:30:40 +0330

    update profile.dust file

--- backend/backend-init.js
+++ backend/backend-init.js
@@ -15,13 +15,15 @@ backend.use(body_parser.json());
 // Backend file uplaod
 backend.use(file_upload());
 
+// Backend other configs
+backend_allowd_urls = ['/login/', '/recovery/', '/recovery/verify/'];
+backend_allowd_avatars = ['png', 'jpeg', 'jpg', 'gif'];
+backend_limited_avatars_size = 1024; // KB
+backend_upload_dir = `${config.app_dir}backend/templates/${config.backend_tmp}/assets/media/`;
+
 // Backend Local variables using in Dust template engine
 backend.locals.backend_url = config.backend_url;
 backend.locals.debug_mode = config.debug_mode;
 backend.locals.captcha_url = config.captcha_url;
 backend.locals.zend_cms_version = "1.0.0";
-
-backend_allowd_urls = ['/login/', '/recovery/', '/recovery/verify/'];
-backend_allowd_avatars = ['png', 'jpeg', 'jpg', 'gif'];
-backend_limited_avatars_size = 1024; // KB
-backend_upload_dir = `${config.app_dir}backend/templates/${config.backend_tmp}/assets/media/`;
+backend.locals.limited_avatars_size = (backend_limited_avatars_size/1024).toString();
