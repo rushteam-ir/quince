56a5df3d3d3d4c0b9ee88d1f8c457510cd293fb9
MegaNoob-stack <kasrahabibbeygi@gmail.com>
2020-03-18 01:58:11 -0700

    fix bug

--- backend/templates/zend/assets/js/dashboard.js
+++ backend/templates/zend/assets/js/dashboard.js
@@ -59,6 +59,8 @@ function shop_dropdown() {
         dashbord_text.style.color = "#B1B1B1";
         shop_field.style.backgroundColor = "#1E1E2D";
         shop_icon.style.color = '#494B74';
+        shop_arrow.style.transform = "rotate(0deg)";
+
 
     } else {
 
@@ -66,6 +68,7 @@ function shop_dropdown() {
         dashbord_text.style.color = "white";
         shop_field.style.background = "#1A1A2C";
         shop_icon.style.color = '#5D78FF';
+        shop_arrow.style.transform = "rotate(-90deg)";
 
     }
 }
@@ -85,3 +88,15 @@ function notifDropDown() {
 }
 
 
+// when click on profile tab on dashbord , tab1 open 
+function profileBtn(){
+    sessionStorage.text = 1;
+    admin_pro_edit.style.display = "block"
+    admin_change_pass.style.display = "none";
+    second_profile_header_number.style.color = "#5D78FF";
+    second_pro_box.style.backgroundColor = "#F7F8FB";
+    second_profile_header_number.style.backgroundColor = "#DFE2EE";
+    first_pro_box.style.backgroundColor = "white";
+    first_profile_header_number.style.color = "white";
+    first_profile_header_number.style.backgroundColor = "#5D78FF";
+}
