11d26ae81cd74509ebd1141158b3d2745b7465da
MegaNoob-stack <kasrahabibbeygi@gmail.com>
2020-03-21 07:25:47 -0700

    asdasdasd

--- backend/templates/zend/assets/js/store-list.js
+++ backend/templates/zend/assets/js/store-list.js
@@ -1,27 +1,41 @@
-function productModal(){
+var product_modal_id = document.getElementById('product_modal_id');
+  
+  function productModal(){
 
-}
+    product_modal_id.style.display = "block";
 
+    new WOW().init();
 
+    $(document).ready(function(){
 
-$(document).ready(function(){
-    $('.slick-field').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.slick-field-1',
-        autoplay:false,
-      });
-      $('.slick-field-1').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        asNavFor: '.slick-field',
-        autoplay:false,
-        focusOnSelect: true,
-        autoplaySpeed: 2000,
-        arrows:false,
-      });
-              
-  });
-      
+      $('.slick-field').slick({
+  
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          fade: true,
+          asNavFor: '.slick-field-1',
+          autoplay:false,
+  
+        });
+  
+        $('.slick-field-1').slick({
+  
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          asNavFor: '.slick-field',
+          autoplay:false,
+          focusOnSelect: true,
+          autoplaySpeed: 2000,
+          arrows:false,
+          centerMode: true,
+  
+        });
+                
+    });
+
+  }
+
+  function removeModal(){
+    product_modal_id.style.display = "none";
+  }
