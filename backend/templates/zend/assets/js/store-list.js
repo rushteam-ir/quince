fed55371ad53fec67631a893aefe626963e83896
MegaNoob-stack <kasrahabibbeygi@gmail.com>
2020-03-24 06:09:40 -0700

    img slider

--- backend/templates/zend/assets/js/store-list.js
+++ backend/templates/zend/assets/js/store-list.js
@@ -1,3 +1,4 @@
+// sweet alert
 $('.product_del').on('click', function (e) {
 
     let product_id = $(e.currentTarget).attr('name');
@@ -25,8 +26,29 @@ $('.product_del').on('click', function (e) {
             })
         }
     })
-})
+});
 
 function redirect(url) {
     location.href = url
 }
+
+// modal slider
+  $(document).ready(function(){
+    $('.main_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav',
+        autoplay:true
+      });
+      $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.main_slider',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true,
+        arrows:false
+      });
+  });
