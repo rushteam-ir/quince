a4e4b29f60294406de870d6f5147fd31aa6deea2
meganoob <kasrahabibbeygi@gmail.com>
2020-04-03 08:20:25 -0700

    creat contact us fornt

---
+++ frontend/templates/melorin/assets/js/contact-us.js
@@ -0,0 +1,20 @@
+var toggle_box = document.getElementsByClassName('header_and_angle_field');
+var i;
+
+for (i = 0; i < toggle_box.length; i++) {
+
+    toggle_box[i].addEventListener("click", function () {
+
+        var panel = this.nextElementSibling;
+        
+        if (panel.style.display === "block") {
+
+            panel.style.display = "none";
+
+        } else {
+
+            panel.style.display = "block";
+            
+        }
+    });
+}
