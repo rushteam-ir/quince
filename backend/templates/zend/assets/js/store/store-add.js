f5c758e1e97be0bae1c4ade126ab2227bf8166d3
meganoob <kasrahabibbeygi@gmail.com>
2020-04-08 05:20:48 -0700

    little chages

--- backend/templates/zend/assets/js/store/store-add.js
+++ backend/templates/zend/assets/js/store/store-add.js
@@ -1,100 +1,3 @@
-// function storeAddError() {
-
-//     let error_text = document.getElementById('store_add_error');
-//     let title_inp = document.forms['store_add_form']['title_inp'].value;
-//     let parent_inp = document.forms['store_add_form']['parent_inp'].value;
-//     let child_inp = document.forms['store_add_form']['child_inp'].value;
-//     let describe = document.forms['store_add_form']['describe'].value;
-//     let price_inp = document.forms['store_add_form']['price_inp'].value;
-//     let inventory_inp = document.forms['store_add_form']['inventory_inp'].value;
-//     let discount_inp = document.forms['store_add_form']['discount_inp'].value;
-//     let product_main_image = document.forms['store_add_form']['product_main_image'].value;
-
-
-//     if (title_inp === "") {
-
-//         error_text.style.display = "block";
-//         document.body.scrollTop = 0;
-//         document.documentElement.scrollTop = 0;
-//         error_text.innerHTML = "لطفا عنوان محصول را انتخاب کنید";
-//         return false;
-
-//     } else if (parent_inp === "") {
-
-//         error_text.style.display = "block";
-//         error_text.innerHTML = "لطفا دسته را انتخاب کنید";
-//         document.body.scrollTop = 0;
-//         document.documentElement.scrollTop = 0;
-//         return false;
-
-//     } else if (child_inp === "0") {
-
-//         error_text.style.display = "block";
-//         error_text.innerHTML = "لطفا زیر دسته را انتخاب کنید";
-//         document.body.scrollTop = 0;
-//         document.documentElement.scrollTop = 0;
-//         return false;
-
-//     } else if (describe === "") {
-
-//         error_text.style.display = "block";
-//         error_text.innerHTML = "لطفا توضیحات محصول را بنویسید";
-//         document.body.scrollTop = 0;
-//         document.documentElement.scrollTop = 0;
-//         return false;
-
-//     } else if (price_inp === "") {
-
-//         error_text.style.display = "block";
-//         error_text.innerHTML = "لطفا قیمت محصول را وارد کنید";
-//         document.body.scrollTop = 0;
-//         document.documentElement.scrollTop = 0;
-//         return false;
-
-//     } else if (inventory_inp === "") {
-
-//         error_text.style.display = "block";
-//         error_text.innerHTML = "لطفا موجودی انبار را وارد کنید";
-//         document.body.scrollTop = 0;
-//         document.documentElement.scrollTop = 0;
-//         return false;
-
-//     } else if (discount_inp === "") {
-
-//         error_text.style.display = "block";
-//         error_text.innerHTML = "لطفا تخفیف را وارد کنید";
-//         document.body.scrollTop = 0;
-//         document.documentElement.scrollTop = 0;
-//         return false;
-
-//     } else if (discount_inp > 100) {
-
-//         error_text.style.display = "block";
-//         error_text.innerHTML = "تخفیف بیشتر از 100 درصد مجاز نمی باشد";
-//         document.body.scrollTop = 0;
-//         document.documentElement.scrollTop = 0;
-//         return false;
-
-//     } else if (product_main_image === "") {
-
-//         error_text.style.display = "block";
-//         error_text.innerHTML = "لطفا عکس اصلی را آپلود کنید";
-//         document.body.scrollTop = 0;
-//         document.documentElement.scrollTop = 0;
-//         return false;
-
-//     } else if (product_main_image === "") {
-
-//         error_text.style.display = "block";
-//         error_text.innerHTML = "لطفا عکس اصلی را آپلود کنید";
-//         document.body.scrollTop = 0;
-//         document.documentElement.scrollTop = 0;
-//         return false;
-
-//     }
-
-// }
-
 // input add & remove
 $(document).ready(function () {
 
@@ -126,6 +29,9 @@ $(document).ready(function () {
 });
 
 
+
+
+
 // chose file customize
 (function () {
 
@@ -148,77 +54,4 @@ $(document).ready(function () {
 
 $(function () {
     $('[data-toggle="popover"]').popover()
-})
-
-
-$(document).ready(function () {
-
-    if (window.File && window.FileList && window.FileReader) {
-
-        $("#files").on("change", function (e) {
-
-            var files = e.target.files,
-                filesLength = files.length;
-            for (var i = 0; i < filesLength; i++) {
-
-                var f = files[i]
-                var fileReader = new FileReader();
-                fileReader.onload = (function (e) {
-
-                    var file = e.target;
-                    $("<div class=\"col-4 pr-0 mt-3\">"+
-                        
-                        "<span class=\"pip\">" +
-
-                        "<img class=\"imageThumb\" src=\"" + e.target.result + "\" title=\"" + file.name + "\"/>" +
-
-                        "<br/><span class=\"remove\"></span>" +
-
-                        "</span>"+
-                        
-                        "</div>").insertAfter("#files");
-
-                    $(".remove").click(function () {
-                        $(this).parents(".col-4").remove();
-                    });
-
-                });
-
-                fileReader.readAsDataURL(f);
-            }
-        });
-
-
-
-    } else {
-
-        alert("Your browser doesn't support to File API")
-    }
-});
-
-$(document).ready(function () {
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            let wrapper = $('.main-row');
-            let fieldHTML = '<div class="col-4 pr-0 mt-3 main-image-show"><span class="pip"><img id="main-image-show" class="imageThumb" src="#"><br><span class="remove"><span><span></div>'
-            reader.onload = function (e) {
-                if (!$(".main-image-show")[0]){
-                    $(wrapper).append(fieldHTML);
-                }
-                $('#main-image-show').attr('src', e.target.result);
-            }
-
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
-
-    $("#main_pic_files").change(function () {
-        readURL(this);
-    });
-
-    $(".remove").click(function () {
-        $(this).parents(".col-4").remove();
-    });
-
 })
