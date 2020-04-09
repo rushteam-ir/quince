f99825ddd26a197081766fe13c0ecf50bedc6c2d
rouzbehsbz <polybius1999@gmail.com>
2020-04-09 16:37:22 +0430

    working on image uplaoder

--- backend/templates/zend/assets/js/store/store-add.js
+++ backend/templates/zend/assets/js/store/store-add.js
@@ -43,18 +43,40 @@ $(document).ready(function () {
         $input.on('change', function (element) {
             var fileName = '';
             if (element.target.value) fileName = element.target.value.split('\\').pop();
-            fileName ? $label.addClass('has-file').find('.js-fileName').html(fileName) : $label.removeClass(
-                'has-file').html(labelVal);
+            fileName ? $label.addClass('has-file').find('.js-fileName').html(fileName) : $label.removeClass('has-file').html(labelVal);
+
         });
     });
 
 })();
+$( document ).ready(function() {
 
-// remove img of browse
-$(".remove"). click(function () {
-    $(this).parent(".remove_parent").remove();
+    // show img when browse
+    $(".input-file-custom").change(function() {
+
+        let input = this;
+
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function(e) {
+
+                let fieldHTML = '<div class="remove_parent"><img class="uploading_img_from_brows" src="'+e.target.result+'"> <div class="remove_img_icon remove"></div></div>';
+                $('.img-box').append(fieldHTML);
+
+            }
+
+            reader.readAsDataURL(input.files[0]); // convert to base64 string
+        }
+
+    });
+
+    // remove img of browse
+    $(".remove"). click(function () {
+        alert('hi')
+    });
+
 });
-
 
 
 // pop over function
