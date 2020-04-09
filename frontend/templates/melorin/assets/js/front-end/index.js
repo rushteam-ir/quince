0aa939f8b2ee22624c85d9ea0b17ca6a09b65021
meganoob <kasrahabibbeygi@gmail.com>
2020-04-09 07:42:04 -0700

    little chages

--- frontend/templates/melorin/assets/js/front-end/index.js
+++ frontend/templates/melorin/assets/js/front-end/index.js
@@ -1,27 +1,27 @@
-//var & let
+// //var & let
 
-var box_of_support_list = document.getElementById('box_of_support_list');
-var myOverlay = document.getElementById("myOverlay");
+// var box_of_support_list = document.getElementById('box_of_support_list');
+// var myOverlay = document.getElementById("myOverlay");
 
 
-//style with js
-box_of_support_list.style.display="none";
+// //style with js
+// box_of_support_list.style.display="none";
 
-//function
-function support_click_drop_down_menu() {
-    if (box_of_support_list.style.display==="none"){
-        box_of_support_list.style.display="block";
-        new WOW().init();
-    }
-    else {
-        box_of_support_list.style.display="none";
-    }
-}
+// //function
+// function support_click_drop_down_menu() {
+//     if (box_of_support_list.style.display==="none"){
+//         box_of_support_list.style.display="block";
+//         new WOW().init();
+//     }
+//     else {
+//         box_of_support_list.style.display="none";
+//     }
+// }
 
-function openSearch() {
-    myOverlay.style.display = "block";
-}
+// function openSearch() {
+//     myOverlay.style.display = "block";
+// }
 
-function closeSearch() {
-    myOverlay.style.display = "none";
-}
+// function closeSearch() {
+//     myOverlay.style.display = "none";
+// }
