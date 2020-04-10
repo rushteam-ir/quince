4b2a8d781bc34eb789f282baa9d3ba9877400dae
meganoob <kasrahabibbeygi@gmail.com>
2020-04-10 08:04:36 -0700

    add more alert

--- backend/templates/zend/assets/js/general/error.js
+++ backend/templates/zend/assets/js/general/error.js
@@ -1,5 +1,17 @@
-function removeError(){
+function removeSuccessError(){
 
-    var error_field = document.getElementById('error_field').style.display = "none";
+    var error_field_success = document.getElementById('error_field_success').style.display = "none";
+    
+}
+
+function removeDangerError(){
+
+    var error_field_danger = document.getElementById('error_field_danger').style.display = "none";
+    
+}
+
+function removeWarningError(){
+
+    var error_field_warning = document.getElementById('error_field_warning').style.display = "none";
     
 }
