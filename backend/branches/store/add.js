521622728726d6bf6049ef1a94674347a685d98c
rouzbehsbz <polybius1999@gmail.com>
2020-04-12 12:36:47 +0430

    fix add query error

--- backend/branches/store/add.js
+++ backend/branches/store/add.js
@@ -95,7 +95,12 @@ router.post('/', async(req,res)=>{
                 let main_image = req.files.product_main_image;
                 let other_images = req.files['product_other_images[]'];
 
-                if(other_images.length < 3){
+                if(!req.files['product_other_images[]']){
+
+                    return res.redirect(`${config.backend_url}store/add/?msg=few-images`);
+
+                }
+                else if(other_images.length < 3){
 
                     return res.redirect(`${config.backend_url}store/add/?msg=few-images`);
 
