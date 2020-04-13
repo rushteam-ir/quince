c2cb058580bd228f35cab1bde127b7ee23ae1122
meganoob <kasrahabibbeygi@gmail.com>
2020-04-13 10:56:45 -0700

    creat discount page

--- backend/templates/zend/assets/js/store/store-discounts.js
+++ backend/templates/zend/assets/js/store/store-discounts.js
@@ -1,32 +1,8 @@
-function discountError(){
+// function discountError(){
 
-    $('.alert_query').remove();
+//     $('.alert_query').remove();
 
-    var value_inp = document.forms['discount_form']['value_inp'].value;
-    var error_field = document.getElementById('error_field_warning_front');
-    var error_text = document.getElementById('error_text')
-
-    if(value_inp == ""){
-
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-        error_field.style.display = "block";
-        error_text.innerHTML = "لطفا میزان تخفیف را وارد کنید .";
-        return false;
-
-    }
-
-    else if(value_inp >100){
-
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-        error_field.style.display = "block";
-        error_text.innerHTML = "لطفا عدد تخفیف را وارد کنید.";
-        return false;
-
-    }
-
-}
+// }
 
 // Delete discount code
 $('.remove_discount').on('click', function (e) {
