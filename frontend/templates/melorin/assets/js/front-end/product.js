4009cdbd5b31490408bf2fc1ddb7577ffe7f70c7
nadie-najm <nadi.codes@gmail.com>
2020-04-13 22:05:40 +0430

    product

--- frontend/templates/melorin/assets/js/front-end/product.js
+++ frontend/templates/melorin/assets/js/front-end/product.js
@@ -1,3 +1,5 @@
+//
+
 let current = document.getElementById('current');
 let thumbnails = document.querySelectorAll('.thumb');
 
@@ -8,3 +10,36 @@ thumbnails.forEach( (e)=> {
         current.setAttribute('src',getsrc);
     });
 });
+
+
+//Similar Product Slider
+
+$(document).ready(function(){
+    $('.slick_slider_for_product').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
+
+
+
+
