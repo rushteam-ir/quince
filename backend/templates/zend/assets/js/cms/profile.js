fb140f20a7fbe4b84eaf240d95994d0fc265239a
meganoob <kasrahabibbeygi@gmail.com>
2020-04-14 05:37:42 -0700

    fix avatar img

--- backend/templates/zend/assets/js/cms/profile.js
+++ backend/templates/zend/assets/js/cms/profile.js
@@ -95,13 +95,36 @@ function redirect(url) {
         var $input = $(this),
             $label = $input.next('.js-labelFile'),
             labelVal = $label.html();
-
-        $input.on('change', function (element) {
-            var fileName = '';
-            if (element.target.value) fileName = element.target.value.split('\\').pop();
-            fileName ? $label.addClass('has-file').find('.js-fileName').html(fileName) : $label.removeClass(
-                'has-file').html(labelVal);
-        });
     });
 
 })();
+$(document).ready(function () {
+
+    // show img when browse
+    $(".input-file-custom").change(function () {
+
+        let input = this;
+
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+
+                let fieldHTML = '<div class="remove_parent"><img class="uploading_img_from_brows" src="' + e.target.result + '"> <div class="remove_img_icon remove"></div></div>';
+                $(input).prev().append(fieldHTML);
+
+            }
+
+            reader.readAsDataURL(input.files[0]); // convert to base64 string
+        }
+
+    });
+
+});
+
+$(document).on('click', '.remove', function (e) {
+
+    $(this).parents('.img-show').next().val('');
+    $(this).parent('.remove_parent').remove();
+
+});
