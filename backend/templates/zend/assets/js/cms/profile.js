3e417304434a6024ac11ebdecbf3fa8501114bbc
rouzbehsbz <polybius1999@gmail.com>
2020-04-14 18:10:38 +0430

    fix avatar upload image

--- backend/templates/zend/assets/js/cms/profile.js
+++ backend/templates/zend/assets/js/cms/profile.js
@@ -56,10 +56,13 @@ function secondChangeDisplay() {
 
 // sweetalert
 
-function deleteCall() {
+$('.avatar_del').on('click', function (e) {
+
+    let image_id = $(e.currentTarget).attr('name');
+
     Swal.fire({
         title: 'حذف تصویر',
-        text: "آیا از حذف تصویر پروفایل خود مطمئن هستید ؟",
+        text: "آیا از حذف این تصویر مطمئن هستید ؟",
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
@@ -69,19 +72,18 @@ function deleteCall() {
     }).then((result) => {
         if (result.value) {
             Swal.fire({
-                title: 'حذف شد',
-                text: 'تصویر پروفایل شما با موفقیت حذف شد برای تایید نهایی کلیک کنید',
-                icon: 'success',
+                title : 'حذف شد',
+                text : 'تصویر مور نظر با موفقیت حذف شد برای تایید نهایی کلیک کنید',
+                icon : 'success',
                 confirmButtonText: 'تایید',
-            }).then((result) => {
+            }).then((result)=>{
                 if (result.value) {
                     redirect(`${backend_url}profile/api/delete-avatar`);
                 }
             })
         }
     })
-    return false
-}
+});
 
 function redirect(url) {
     location.href = url
