2dce3ad20634fb761a989cee2cee210ac8005de4
nadie-najm <nadi.codes@gmail.com>
2020-04-19 17:35:53 +0430

    product edit

--- frontend/templates/melorin/assets/js/front-end/product.js
+++ frontend/templates/melorin/assets/js/front-end/product.js
@@ -3,7 +3,6 @@
 let current = document.getElementById('current');
 let thumbnails = document.querySelectorAll('.thumb');
 let explain_id=  document.getElementById('explain');
-let further_explain_id=  document.getElementById('further_explain');
 let comments_product_id=  document.getElementById('comments_product');
 let add_comments_id=  document.getElementById('add_comments');
 
@@ -49,31 +48,21 @@ var swiper = new Swiper('.main_product_swiper', {
 function explain() {
     sessionStorage.text=1;
     explain_id.style.display = "block";
-    further_explain_id.style.display = "none";
     comments_product_id.style.display = "none";
     add_comments_id.style.display = "none";
 }
 
-function further_explain() {
-    sessionStorage.text=2;
-    explain_id.style.display = "none";
-    further_explain_id.style.display = "block";
-    comments_product_id.style.display = "none";
-    add_comments_id.style.display = "none";
-}
 
 function comments_product() {
-    sessionStorage.text=3;
+    sessionStorage.text=2;
     explain_id.style.display = "none";
-    further_explain_id.style.display = "none";
     comments_product_id.style.display = "block";
     add_comments_id.style.display = "none";
 }
 
 function add_comments() {
-    sessionStorage.text=4;
+    sessionStorage.text=3;
     explain_id.style.display = "none";
-    further_explain_id.style.display = "none";
     comments_product_id.style.display = "none";
     add_comments_id.style.display = "block";
 }
@@ -82,11 +71,8 @@ function add_comments() {
 if (sessionStorage.text==1){
     explain();
 }
-else if (sessionStorage.text==2){
-    further_explain();
-}
 
-else if (sessionStorage.text==3){
+else if (sessionStorage.text==2){
     comments_product();
 }
 else {
