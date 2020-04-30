29b801b4c0d4bd1a00509a2b520db3f8c36bad07
meganoob <kasrahabibbeygi@gmail.com>
2020-04-30 03:46:14 -0700

    optimize js

--- backend/templates/toffee/assets/js/login/login.js
+++ backend/templates/toffee/assets/js/login/login.js
@@ -1,5 +1,5 @@
 let recovery_page = document.getElementById('recovery_page');
-let log_field = document.querySelector('.main_filed_of_login');
+let mainBox = document.querySelector('.main_filed_of_login');
 
 let back_btn_log = document.querySelector('.back_btn_log');
 let back_to_recovery = document.querySelector('.back_to_recovery');
@@ -7,25 +7,27 @@ let go_verify = document.querySelector('.go_verify');
 
 let recovery_form = document.querySelector('.recovery_form');
 let log_form = document.querySelector('.log_form');
-let recovery_verify_form = document.querySelector('.recovery_verify_form');
+let verify_form = document.querySelector('.recovery_verify_form');
 
-if(sessionStorage.recovery == 2){
-    
+if (sessionStorage.recovery == 2) {
+
     recovery_form.style.display = "block";
     log_form.style.display = "none";
     recovery_form.style.opacity = "1";
-    log_field.style.height = "400px";
-    recovery_verify_form.style.display = "none";
+    mainBox.style.height = "400px";
+    verify_form.style.display = "none";
+    mainBox.style.transition = "none";
 
 }
 
-if(sessionStorage.recovery == 3){
+if (sessionStorage.recovery == 3) {
 
     log_form.style.display = "none";
-    recovery_verify_form.style.display = "block";
+    verify_form.style.display = "block";
     recovery_form.style.display = "none";
-    recovery_verify_form.style.opacity = "1";
-    log_field.style.height = "350px";
+    verify_form.style.opacity = "1";
+    mainBox.style.height = "350px";
+    mainBox.style.transition = "none";
 
 }
 
@@ -34,93 +36,93 @@ if(sessionStorage.recovery == 3){
 recovery_page.addEventListener('click', function () {
 
     sessionStorage.recovery = 2;
-
     log_form.style.opacity = "0";
+    mainBox.style.transition = "all ease-out 0.2s";
 
     setTimeout(function () {
 
         recovery_form.style.display = "block";
         log_form.style.display = "none";
-        log_field.style.height = "400px";
+        mainBox.style.height = "400px";
 
     }, 400);
 
-    setTimeout(function(){
+    setTimeout(function () {
 
         recovery_form.style.opacity = "1";
-        
+
     }, 500);
 
 });
 
 // back to log
 
-back_btn_log.addEventListener('click',function(){
+back_btn_log.addEventListener('click', function () {
 
     sessionStorage.recovery = 1;
-
     recovery_form.style.opacity = "0";
+    mainBox.style.transition = "all ease-out 0.2s";
 
     setTimeout(function () {
 
         recovery_form.style.display = "none";
         log_form.style.display = "block";
-        log_field.style.height = "450px";
+        mainBox.style.height = "450px";
 
     }, 400);
 
-    setTimeout(function(){
+    setTimeout(function () {
 
         log_form.style.opacity = "1";
-        
+
     }, 500);
 
 });
 
 // go to verify
 
-go_verify.addEventListener('click',function(){
+go_verify.addEventListener('click', function () {
 
     sessionStorage.recovery = 3;
-
     recovery_form.style.opacity = "0";
+    mainBox.style.transition = "all ease-out 0.2s";
 
     setTimeout(function () {
 
         recovery_form.style.display = "none";
-        recovery_verify_form.style.display = "block";
-        log_field.style.height = "350px";
+        verify_form.style.display = "block";
+        mainBox.style.height = "350px";
 
     }, 400);
 
-    setTimeout(function(){
+    setTimeout(function () {
 
-        recovery_verify_form.style.opacity = "1";
-        
+        verify_form.style.opacity = "1";
+
     }, 500);
 
 });
 
 //back to recovery
 
-back_to_recovery.addEventListener('click',function(){
+back_to_recovery.addEventListener('click', function () {
 
     sessionStorage.recovery = 2;
-
-    recovery_verify_form.style.opacity = "0";
+    verify_form.style.opacity = "0";
+    mainBox.style.transition = "all ease-out 0.2s";
 
     setTimeout(function () {
 
         recovery_form.style.display = "block";
-        recovery_verify_form.style.display = "none";
-        log_field.style.height = "400px";
+        verify_form.style.display = "none";
+        mainBox.style.height = "400px";
 
     }, 400);
 
-    setTimeout(function(){
+    setTimeout(function () {
 
         recovery_form.style.opacity = "1";
-        
+
     }, 500);
 
 });
