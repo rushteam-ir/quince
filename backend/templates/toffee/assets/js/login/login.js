36c3647c64466c3ab12bd69acb3b33ec1b9ee0dd
meganoob <kasrahabibbeygi@gmail.com>
2020-05-02 10:47:44 -0700

    add animation

--- backend/templates/toffee/assets/js/login/login.js
+++ backend/templates/toffee/assets/js/login/login.js
@@ -11,38 +11,58 @@ function loginError() {
 
     if (email_inp == '') {
 
-        error_box.style.display = "block";
+        error_box.style.display = 'block';
         error_text.innerText = 'لطفا ایمیل خود را وارد کنید.';
         return false;
 
     } else if (!email_inp.includes('@') || !email_inp.includes('.com') || email_inp.length < 9) {
 
-        error_box.style.display = "block";     
+        error_box.style.display = 'block';     
         error_text.innerText = 'ایمیل وارد شده صحیح نمی باشد.';
         return false;
 
     } else if (password_inp == '') {
          
-        error_box.style.display = "block";
+        error_box.style.display = 'block';
         error_text.innerText = 'لطفا رمز عبور خود را وارد کنید.';
         return false;
 
     } else if (password_inp.length < 8) {
          
-        error_box.style.display = "block";
+        error_box.style.display = 'block';
         error_text.innerText = 'رمز عبور کم تر از حد مجاز است.';
         return false;
 
     } else if (captcha_inp == '') {
          
-        error_box.style.display = "block";
+        error_box.style.display = 'block';
         error_text.innerText = 'لطفا کد امنیتی را وارد کنید';
         return false;
 
     } else if (captcha_inp.length < 5) {
          
-        error_box.style.display = "block";
+        error_box.style.display = 'block';
         error_text.innerText = 'کد امنیتی کم تر از حد مجاز است.';
+        return false;
+
+    }
+
+}
+
+function recoveryError(){
+
+    let email_inp = document.forms['recovery_form']['email_inp'].value;
+
+    if (email_inp == '') {
+
+        error_box.style.display = 'block';
+        error_text.innerHTML = 'لطفا ایمیل خود را وارد کنید.'
+        return false;
+
+    } else if (!email_inp.includes('@') || !email_inp.includes('.com') || email_inp.length < 9) {
+
+        error_box.style.display = 'block';     
+        error_text.innerText = 'ایمیل وارد شده صحیح نمی باشد.';
         return false;
 
     }
@@ -63,29 +83,10 @@ function verifyError() {
 
 }
 
-function recoveryError(){
-
-    let email_inp = document.forms['recovery_form']['recovery_email_inp'].value;
-
-    if (email_inp == '') {
-
-        error_box.style.display = "block";
-        error_text.innerHTML = "لطفا ایمیل خود را وارد کنید."
-        return false;
-
-    }else if (!email_inp.includes('@') || !email_inp.includes('.com') || email_inp.length < 9) {
-
-        error_box.style.display = "block";     
-        error_text.innerText = 'ایمیل وارد شده صحیح نمی باشد.';
-        return false;
-
-    }
-
-}
 
 function removeWarningErrorFront(){
 
-    document.getElementById('error_field_warning_front').style.display = "none";
+    document.getElementById('error_field_warning_front').style.display = 'none';
     
 }
 
