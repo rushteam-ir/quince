2f22c737ba1417fad494d56f2f222ab5c3b9e0b7
meganoob <kasrahabibbeygi@gmail.com>
2020-05-04 07:58:48 -0700

    fix side nav bar and creat navbar

--- backend/templates/quince/assets/js/layout/collaps-menu.js
+++ backend/templates/quince/assets/js/layout/collaps-menu.js
@@ -1,21 +1,27 @@
-var dashboard_collaps = document.getElementsByClassName("dashboard_collaps");
-var i;
+let dashboard_collaps = document.getElementsByClassName('dashboard_collaps');
+let angle_rotate = document.getElementsByClassName('angle_rotate');
+let i;
 
 for (i = 0; i < dashboard_collaps.length; i++) {
 
-    dashboard_collaps[i].addEventListener("click", function () {
+    dashboard_collaps[i].addEventListener('click', function () {
 
-
-        var panel = this.nextElementSibling;
+        this.classList.toggle('active_collapse');
+        let panel = this.nextElementSibling;
+        let panelChildNode = this.childNodes;
 
         if (panel.style.maxHeight) {
 
             panel.style.maxHeight = null;
+            panelChildNode[2].style.transform ='rotate(0deg)';
 
         } else {
 
-            panel.style.maxHeight = panel.scrollHeight + "px";
+            panel.style.maxHeight = panel.scrollHeight + 'px';
+            panelChildNode[2].style.transform ='rotate(-90deg)';
             
         }
+
     });
+
 }
--- backend/templates/quince/assets/js/layout/toggle-dsh.js
+++ backend/templates/quince/assets/js/layout/toggle-dsh.js
@@ -1,9 +1,11 @@
-let dashboard_angle = document.getElementsByClassName('dashboard_angle');
+let dashboard_angle = document.querySelectorAll('.dashboard_angle');
 let dashboard_text = document.querySelectorAll('.dashboard_text');
 let close_dashboard = document.querySelector('.close_dashboard');
 let open_dashboard = document.querySelector('.open_dashboard');
 let dashboard_logo = document.querySelector('.dashboard_logo');
 let dashboard_main = document.querySelector('.dashboard_main');
+let sub_collaps = document.querySelectorAll('.sub_collaps');
+let hover_menu = document.querySelectorAll('.hover_menu');
 let dashboard = document.querySelector('.dashboard');
 
 
@@ -28,14 +30,16 @@ function closeDsh() {
 
     dashboard_main.style.transition = 'all ease-out 0.5s';
     dashboard.style.transition = 'all ease-out 0.5s';
-    open_dashboard.style.display = 'block';
     close_dashboard.style.display = 'none';
+    open_dashboard.style.display = 'block';
     dashboard_logo.style.display = 'none';
-    dashboard_main.style.width = '96%';
-    dashboard.style.width = '4%';
+    dashboard_main.style.width = '97.1%';
+    dashboard.style.width = '2.9%';
     localStorage.close = 'close';
     let i = 0;
     let y = 0;
+    let z = 0;
+    let e = 0;
 
     while (i < dashboard_text.length) {
 
@@ -51,22 +55,38 @@ function closeDsh() {
 
     }
 
+    while (z < sub_collaps.length) {
+
+        sub_collaps[z].style.display = 'none';
+        z++;
+
+    }
+
+    while (e < hover_menu.length) {
+
+        hover_menu[e].style.display = 'block';
+        e++
+
+    }
+
 }
 
 function openDsh() {
 
     dashboard_main.style.transition = 'all ease-out 0.5s';
     dashboard.style.transition = 'all ease-out 0.5s';
-    open_dashboard.style.display = 'none';
     close_dashboard.style.display = 'block';
     dashboard_logo.style.display = 'block';
-    dashboard_main.style.width = '85%';
-    dashboard.style.width = '15%';
+    open_dashboard.style.display = 'none';
+    dashboard_main.style.width = '88%';
+    dashboard.style.width = '12%';
     localStorage.close = 'open';
 
     let y = 0;
+    let z = 0;
+    let e = 0;
 
-    setTimeout(function(){
+    setTimeout(function () {
 
         let i = 0;
 
@@ -77,13 +97,27 @@ function openDsh() {
 
         }
 
-    },140);
+    }, 140);
 
 
     while (y < dashboard_angle.length) {
 
         dashboard_angle[y].style.display = 'inline-block';
         y++;
+
+    }
+
+    while (z < sub_collaps.length) {
+
+        sub_collaps[z].style.display = 'block';
+        z++
+
+    }
+
+    while (e < hover_menu.length) {
+
+        hover_menu[e].style.display = 'none';
+        e++
 
     }
 
