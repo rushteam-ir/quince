22fa11854cec97c53f7137ace2a665df5246f3da
meganoob <kasrahabibbeygi@gmail.com>
2020-05-11 15:57:56 -0700

    fix category

---
+++ backend/templates/quince/assets/js/category/category.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    $('#selectall').change(function () {
+
+        if ($(this).prop('checked')) {
+
+            $('.table_checkbox').prop('checked', true);
+
+        } else {
+
+            $('.table_checkbox').prop('checked', false);
+
+        }
+
+    });
+
+    $('#selectall').trigger('change');
+
+});
