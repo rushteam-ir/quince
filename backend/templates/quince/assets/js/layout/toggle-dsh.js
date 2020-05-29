101d4cb11d34f4be4ba79085291fa2fda66915a5
meganoob <kasrahabibbeygi@gmail.com>
2020-05-29 14:30:40 -0700

    fix dashboard

--- backend/templates/quince/assets/js/layout/toggle-dsh.js
+++ backend/templates/quince/assets/js/layout/toggle-dsh.js
@@ -1,16 +1,17 @@
-
 $(document).ready(function(){
 
     $(document).on('click','.close_dsh_btn',function(){
 
+        dashboard.removeClass('open_dsh');
         dashboard.addClass('close_dsh');
-        main_cms_field.css('width','97.5%');
+        main_cms_field.css('width','96.2%');
 
     });
 
 
     $(document).on('click','.open_dsh_btn',function(){
 
+        dashboard.removeClass('close_dsh');
         dashboard.addClass('open_dsh');
         main_cms_field.css('width','85%');
 
