5e566d3ea5e66a0e501e3bff74360475da518c40
meganoob <kasrahabibbeygi@gmail.com>
2020-05-31 10:21:09 -0700

    creat user list

---
+++ backend/templates/quince/assets/js/article/article-add.js
@@ -0,0 +1,73 @@
+(function () {
+
+    $('.input-file-custom').each(function () {
+        var $input = $(this),
+            $label = $input.next('.js-labelFile'),
+            labelVal = $label.html();
+    });
+
+})();
+
+$(document).ready(function () {
+
+    // show img when browse
+    $(".input-file-custom").change(function () {
+
+        let input = this;
+
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+
+                let fieldHTML = '<div class="remove_parent"><img class="uploading_img_from_brows" src="' + e.target.result + '"> <div class="remove_img_icon remove"></div></div>';
+                $(input).prev().append(fieldHTML);
+
+            }
+
+            reader.readAsDataURL(input.files[0]); // convert to base64 string
+        }
+
+    });
+
+});
+
+$(document).on('click', '.remove', function (e) {
+
+    $(this).parents('.img-show').next().val('');
+    $(this).parent('.remove_parent').remove();
+
+});
+
+$('.avatar_del').on('click', function (e) {
+
+    let image_id = $(e.currentTarget).attr('name');
+
+    Swal.fire({
+        title: 'حذف تصویر',
+        text: "آیا از حذف این تصویر مطمئن هستید ؟",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'بله',
+        cancelButtonText: 'خیر',
+    }).then((result) => {
+        if (result.value) {
+            Swal.fire({
+                title: 'حذف شد',
+                text: 'تصویر مور نظر با موفقیت حذف شد برای تایید نهایی کلیک کنید',
+                icon: 'success',
+                confirmButtonText: 'تایید',
+            }).then((result) => {
+                if (result.value) {
+                    redirect(`${backend_url}profile/api/delete-avatar`);
+                }
+            })
+        }
+    })
+});
+
+function redirect(url) {
+    location.href = url
+}
