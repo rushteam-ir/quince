e8bca28f5ca995a7ddd32c689896d0d75ba82d40
rouzbehsbz <polybius1999@gmail.com>
2020-06-16 01:38:17 +0430

    fix article wysiwyg issue

--- backend/templates/quince/assets/js/article/article-add.js
+++ backend/templates/quince/assets/js/article/article-add.js
@@ -72,7 +72,7 @@ function redirect(url) {
     location.href = url
 }
 
-// Froala Editor Options
+// WYSIWYG Editor Options
 
 new FroalaEditor('textarea', {
 
@@ -116,56 +116,6 @@ new FroalaEditor('textarea', {
 
 })
 
-/*
-tinymce.init({
-    selector: 'textarea',
-    themes: 'modern',
-    height: 200,
-    plugins: "image media",
-    file_picker_callback: function(callback, value, meta) {
-        // File type
-        if (meta.filetype =="media" || meta.filetype =="image") {
-
-            // Trigger click on file element
-            jQuery("#fileupload").trigger("click");
-            $("#fileupload").unbind('change');
-            // File selection
-            jQuery("#fileupload").on("change", function() {
-                var file = this.files[0];
-                var reader = new FileReader();
-
-                // FormData
-                var fd = new FormData();
-                var files = file;
-                fd.append("file",files);
-                fd.append('filetype',meta.filetype);
-
-                var filename = "";
-
-                // AJAX
-                jQuery.ajax({
-                    url: "upload.php",
-                    type: "post",
-                    data: fd,
-                    contentType: false,
-                    processData: false,
-                    async: false,
-                    success: function(response){
-                        filename = response;
-                    }
-                });
-
-                reader.onload = function(e) {
-                    callback("upload/"+filename);
-                };
-                reader.readAsDataURL(file);
-            });
-        }
-
-    }
-
-})
-*/
 // Get Sub Categories
 $(document).ready(function () {
 
