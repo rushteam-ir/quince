31f3b49313f742ec52638b44e460dca7ec7f9b1d
meganoob <kasrahabibbeygi@gmail.com>
2020-07-15 04:00:53 -0700

    asdasd

--- backend/templates/quince/assets/js/profile/profile-tabs.js
+++ backend/templates/quince/assets/js/profile/profile-tabs.js
@@ -35,6 +35,14 @@ $(document).ready(function () {
 
     });
 
+    profile_tab.eq('3').click(function () {
+
+        pro_field.hide();
+        pro_field.eq('3').fadeIn();
+        sessionStorage.profileTab = '4';
+
+    });
+
 
     if(sessionStorage.profileTab =='1'){
 
@@ -60,6 +68,15 @@ $(document).ready(function () {
         profile_tab.eq('2').addClass('profile_tab_active');
         pro_field.hide()
         pro_field.eq('2').css('display', 'block');
+    
+    }
+
+    else if(sessionStorage.profileTab == '4'){
+
+        profile_tab.removeClass('profile_tab_active');
+        profile_tab.eq('3').addClass('profile_tab_active');
+        pro_field.hide()
+        pro_field.eq('3').css('display', 'block');
     
     }
 
--- my component/js/component.js
+++ my component/js/component.js
@@ -17,13 +17,13 @@ $('#selectall').change(function () {
 
 // images modal
 
-$('.QT_image img').click(function(){
+$('.QT_image img').click(function () {
 
     $('.image_modal_field').fadeIn();
 
 });
 
-$('.remove_modal').click(function(){
+$('.remove_modal').click(function () {
 
     $('.image_modal_field').fadeOut();
 
@@ -31,43 +31,52 @@ $('.remove_modal').click(function(){
 
 // upload custom
 
-(function () {
+// (function () {
 
-    $('.input-file-custom').each(function () {
-        var $input = $(this),
-            $label = $input.next('.js-labelFile'),
-            labelVal = $label.html();
-    });
+//     $('.input-file-custom').each(function () {
+//         var $input = $(this),
+//             $label = $input.next('.js-labelFile'),
+//             labelVal = $label.html();
+//     });
 
-})();
+// })();
 
-$(document).ready(function () {
+// $(document).ready(function () {
 
-    // show img when browse
-    $(".input-file-custom").change(function () {
+//     // show img when browse
+//     $(".input-file-custom").change(function () {
 
-        let input = this;
+//         let input = this;
 
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
+//         if (input.files && input.files[0]) {
+//             var reader = new FileReader();
 
-            reader.onload = function (e) {
+//             reader.onload = function (e) {
 
-                let fieldHTML = '<div class="remove_parent"><img class="uploading_img_from_brows" src="' + e.target.result + '"> <div class="remove_img_icon remove"></div></div>';
-                $(input).prev().append(fieldHTML);
+//                 let fieldHTML = '<div class="remove_parent"><img class="uploading_img_from_brows" src="' + e.target.result + '"> <div class="remove_img_icon remove"></div></div>';
+//                 $(input).prev().append(fieldHTML);
 
-            }
+//             }
 
-            reader.readAsDataURL(input.files[0]);
-        }
+//             reader.readAsDataURL(input.files[0]);
+//         }
 
-    });
+//     });
 
-});
+// });
 
-$(document).on('click', '.remove', function (e) {
+// $(document).on('click', '.remove', function (e) {
 
-    $(this).parents('.img-show').next().val('');
-    $(this).parent('.remove_parent').remove();
+//     $(this).parents('.img-show').next().val('');
+//     $(this).parent('.remove_parent').remove();
 
-});
+// });
+
+
+// drop down
+
+$('.dd_menu').click(function(){
+
+    $('.dropdown_menu').toggle()
+
+})
