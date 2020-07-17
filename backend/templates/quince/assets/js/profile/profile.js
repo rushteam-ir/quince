c3fac260e02de1cdbac29738022406fe08793fda
meganoob <kasrahabibbeygi@gmail.com>
2020-07-17 09:52:52 -0700

    fix profile rote

--- backend/templates/quince/assets/js/profile/profile.js
+++ backend/templates/quince/assets/js/profile/profile.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 
             reader.onload = function (e) {
 
-                let fieldHTML = '<div class="remove_parent"><img class="uploading_img_from_brows" src="' + e.target.result + '"> <div class="remove_img_icon remove"></div> <div class="add_img_icon"></div>  </div>';
+                let fieldHTML = '<div class="remove_parent"><img class="uploading_img_from_brows" src="' + e.target.result + '"> <div class="remove_img_icon remove"></div> <button type="submit" name="change_avatar" class="add_img_icon btn"></button></div>';
                 $(input).prev().append(fieldHTML);
 
             }
