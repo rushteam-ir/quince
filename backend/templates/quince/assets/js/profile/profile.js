22bd305701dae0889b03ab372b821aa1671731ff
meganoob <kasrahabibbeygi@gmail.com>
2020-07-17 09:49:18 -0700

    add image confiq on profile

--- backend/templates/quince/assets/js/profile/profile.js
+++ backend/templates/quince/assets/js/profile/profile.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 
             reader.onload = function (e) {
 
-                let fieldHTML = '<div class="remove_parent"><img class="uploading_img_from_brows" src="' + e.target.result + '"> <div class="remove_img_icon remove"></div></div>';
+                let fieldHTML = '<div class="remove_parent"><img class="uploading_img_from_brows" src="' + e.target.result + '"> <div class="remove_img_icon remove"></div> <div class="add_img_icon"></div>  </div>';
                 $(input).prev().append(fieldHTML);
 
             }
