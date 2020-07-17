a7a0ec3c7b16e75bad671193c66fa53dacff614e
meganoob <kasrahabibbeygi@gmail.com>
2020-07-17 12:02:02 -0700

    fix some bug of component

--- backend/templates/quince/assets/js/profile/profile.js
+++ backend/templates/quince/assets/js/profile/profile.js
@@ -31,3 +31,46 @@ $('.avatar_del').on('click', function (e) {
 function redirect(url) {
     location.href = url
 }
+
+// upload custom
+
+(function () {
+
+    $('.input-file-custom').each(function () {
+        var $input = $(this),
+            $label = $input.next('.js-labelFile'),
+            labelVal = $label.html();
+    });
+
+})();
+
+$(document).ready(function () {
+
+    // show img when browse
+    $(".input-file-custom").change(function () {
+
+        let input = this;
+
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+
+                let fieldHTML = '<div class="remove_parent"><img class="uploading_img_from_brows" src="' + e.target.result + '"> <div class="remove_img_icon remove"></div>  <button class="btn add_img_icon"></button>  </div>';
+                $(input).prev().append(fieldHTML);
+
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+
+    });
+
+});
+
+$(document).on('click', '.remove', function (e) {
+
+    $(this).parents('.img-show').next().val('');
+    $(this).parent('.remove_parent').remove();
+
+});
