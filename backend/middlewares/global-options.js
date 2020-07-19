a283592525cbb7249539e7af3511a3504520c1c5
rouzbehsbz <polybius1999@gmail.com>
2020-07-19 16:43:00 +0430

    comment global optiones midlleware to fix a bug

--- backend/middlewares/global-options.js
+++ backend/middlewares/global-options.js
@@ -4,13 +4,15 @@ router.use(async(req, res, next)=>{
 
     try{
 
-        res.locals.global = {
-
-            admin_info : req.session.admin_info,
-            short_letter_nick_name : req.session.admin_info.nick_name.charAt(0),
-            short_letter_name : req.session.admin_info.first_name.charAt(0),
-
-        }
+        // if(!isUndefined(req.session.admin_info)){
+        //     res.locals.global.admin_info = req.session.admin_info;
+        // }
+        // if(!isUndefined(req.session.admin_info.nick_name)){
+        //     res.locals.global.short_letter_nick_name = req.session.admin_info.nick_name.charAt(0);
+        // }
+        // if(!isUndefined(req.session.admin_info.first_name)){
+        //     res.locals.global.short_letter_name = req.session.admin_info.first_name.charAt(0);
+        // }
 
         next();
 
