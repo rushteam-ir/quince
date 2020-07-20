8b9a6d018d0d7720693bfaa647ff6868fac9250b
meganoob <kasrahabibbeygi@gmail.com>
2020-07-20 04:32:10 -0700

    fix some thing

---
+++ backend/templates/quince/assets/js/article/article-edit/article-edit-error.js
@@ -0,0 +1,34 @@
+let err = new errorHandler();
+
+function articleAddError() {
+
+    let title_inp = $('input[name=title_inp]').val();
+    let describe_inp = $('textarea[name=describe_inp]').val();
+    let meta_describe_inp = $('textarea[name=meta_describe_inp]').val();
+    let main_image = $('input[name=main_image]').val();
+
+    let validation_result = null;
+
+    validation_result = err.initiate([
+
+        { value : title_inp, type : 'empty'},
+        { value : describe_inp, type : 'empty'},
+        { value : meta_describe_inp, type : 'empty'},
+        { value : main_image, type : 'empty'},
+
+    ]);
+
+    if(validation_result){
+
+        error();
+        $('.error_info p').text(validation_result);
+        return false;
+
+    }
+    else{
+
+        return true;
+
+    }
+    
+}
