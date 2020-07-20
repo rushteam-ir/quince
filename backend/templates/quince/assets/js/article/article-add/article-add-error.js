7160d007b3911b649842c127e5bc91776c6a3af9
meganoob <kasrahabibbeygi@gmail.com>
2020-07-20 03:58:09 -0700

    fix article add errors

--- backend/templates/quince/assets/js/article/article-add/article-add-error.js
+++ backend/templates/quince/assets/js/article/article-add/article-add-error.js
@@ -3,12 +3,18 @@ let err = new errorHandler();
 function articleAddError() {
 
     let title_inp = $('input[name=title_inp]').val();
+    let describe_inp = $('textarea[name=describe_inp]').val();
+    let meta_describe_inp = $('textarea[name=meta_describe_inp]').val();
+    let main_image = $('input[name=main_image]').val();
 
     let validation_result = null;
 
     validation_result = err.initiate([
 
         { value : title_inp, type : 'empty'},
+        { value : describe_inp, type : 'empty'},
+        { value : meta_describe_inp, type : 'empty'},
+        { value : main_image, type : 'empty'},
 
     ]);
 
