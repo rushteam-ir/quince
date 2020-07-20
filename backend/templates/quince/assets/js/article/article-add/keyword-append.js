185b913edb32a679d905200d511b2e67807c627a
meganoob <kasrahabibbeygi@gmail.com>
2020-07-19 23:39:53 -0700

    fix check box on profile

--- backend/templates/quince/assets/js/article/article-add/keyword-append.js
+++ backend/templates/quince/assets/js/article/article-add/keyword-append.js
@@ -3,8 +3,14 @@ $('.keyword_btn').click(function(){
 
     let keys_inp = $('input[name=keys_inp]').val();
 
-    let append_element = '<div class="key_word_box"><p>' + keys_inp + '</p><i class="fas fa-times"></i></div>';
+    if(keys_inp !== ''){
 
-    $('.append_keyword').append(append_element);
+        let append_element = '<div class="key_word_box"><p>' + keys_inp + '</p><i class="fas fa-times"></i></div>';
+
+        $('.append_keyword').append(append_element);
+
+        $('input[name=keys_inp]').val('');
+
+    }
 
 });
