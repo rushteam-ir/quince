c86095867910d4de755f5ae95726b72bd19918c8
meganoob <kasrahabibbeygi@gmail.com>
2020-07-19 23:55:20 -0700

    add error to categort

--- backend/templates/quince/assets/js/article/article-add/keyword-append.js
+++ backend/templates/quince/assets/js/article/article-add/keyword-append.js
@@ -5,12 +5,19 @@ $('.keyword_btn').click(function(){
 
     if(keys_inp !== ''){
 
-        let append_element = '<div class="key_word_box"><p>' + keys_inp + '</p><i class="fas fa-times"></i></div>';
+        let append_element = '<div class="key_word_box"><p>' + keys_inp + '</p><i class="fas fa-times test_remove"></i></div>';
 
         $('.append_keyword').append(append_element);
 
         $('input[name=keys_inp]').val('');
 
     }
+
+});
+
+$('.test_remove').click(function(){
+
+    // $('.key_word_box').remove();
+    alert('asdasd')
 
 });
