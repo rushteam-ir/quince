1cec7c16d4f47f1d27c035111365e3635bcd017d
meganoob <kasrahabibbeygi@gmail.com>
2020-08-11 00:56:28 -0700

    fix mixin and fix article uploader

--- backend/templates/quince/assets/js/article/article-add.js
+++ backend/templates/quince/assets/js/article/article-add.js
@@ -44,3 +44,53 @@ $(document).ready(function () {
 
 });
 
+
+// uploader custom
+
+$(".chose_file_inp").change(function(){
+
+    readURL(this);
+
+    $('.show_img').css('display' , 'inline-block');
+
+    let img_name = this.files[0].name;
+
+    // this.files && this.files.length ? this.files[0].name.split('*')[0] : '';
+
+    $('.chose_file_inp').attr('data-value',img_name);
+
+});
+
+$('.remove_main_img').click(function(){
+
+    $('.img_upload_field , .show_img').css('display' , 'none');
+
+    $(".chose_file_inp").val('');
+
+    $('.chose_file_inp').attr('data-value','');
+
+});
+
+function readURL(input) {
+
+    if (input.files && input.files[0]) {
+
+        var reader = new FileReader();
+        
+        reader.onload = function (e) {
+
+            $(input).next().children('img').attr('src', e.target.result);
+
+        }
+
+        reader.readAsDataURL(input.files[0]);
+        
+    }
+
+};
+
+$('.show_img').click(function(){
+
+    $('.img_upload_field').fadeToggle();
+
+});
--- backend/templates/quince/assets/js/article/article-edit.js
+++ backend/templates/quince/assets/js/article/article-edit.js
@@ -72,3 +72,49 @@ $(document).ready(function () {
         }
     })
 });
+
+
+// uploader custom
+
+$(".chose_file_inp").change(function(){
+
+    readURL(this);
+
+    $('.show_img').css('display' , 'inline-block');
+
+    let img_name = this.files && this.files.length ? this.files[0].name.split('*')[0] : '';
+
+    $('.chose_file_inp').attr('data-value',img_name);
+
+    if(img_name == ''){
+
+        $('.img_upload_field').css('display' , 'none');
+        $('.show_img').css('display' , 'none');
+
+    }
+    
+});
+
+function readURL(input) {
+
+    if (input.files && input.files[0]) {
+
+        var reader = new FileReader();
+        
+        reader.onload = function (e) {
+
+            $(input).next().children().attr('src', e.target.result);
+
+        }
+
+        reader.readAsDataURL(input.files[0]);
+        
+    }
+
+};
+
+$('.show_img').click(function(){
+
+    $('.img_upload_field').fadeToggle();
+
+});
