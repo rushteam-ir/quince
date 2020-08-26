d85b1ea5c4625c4fbc04e5bab65ee90f055fa0be
nadie-najm <nadi.codes@gmail.com>
2020-08-26 15:14:22 +0430

    melorin2 layout

---
+++ frontend/templates/melorin2/assets/js/layouts/layouts.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    $('.nav_btn_responsive').click(function () {
+        $('.layer_of_nav').fadeIn();
+    });
+
+    $('.btn_close').click(function () {
+        $('.layer_of_nav').fadeOut();
+    });
+
+
+    $(window).resize(function () {
+        if ( $(window).width()>991 ){
+            $('.layer_of_nav').css('display','none');
+        }
+    });
+
+
+});
+
