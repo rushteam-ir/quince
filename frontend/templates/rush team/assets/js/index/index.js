f3558fd51ec54cdd19d1d72b06be1417842033d7
meganoob <kasrahabibbeygi@gmail.com>
2020-08-27 05:37:20 -0700

    change rush team theam and add some part

--- frontend/templates/rush team/assets/js/index/index.js
+++ frontend/templates/rush team/assets/js/index/index.js
@@ -6,3 +6,12 @@ var swiper = new Swiper('.swiper-container', {
         clickable: true,
     },
 });
+
+new WOW().init();
+
+var typed = new Typed('#MW_typed', {
+    strings: ["برترین نمونه کار های ما را در بالا میتوانید مشاهده کنید . " , "برای مشاهده نمونه کار های بیشتر ب روی دکمه روبرو کلیک کنید . "],
+    typeSpeed: 30,
+    backSpeed: 20,
+    loop: true
+});
