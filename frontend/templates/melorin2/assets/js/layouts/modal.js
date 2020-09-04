3cd9c6af9c94eb88f9d7f6ba8487454e4ce7dca0
nadie-najm <nadi.codes@gmail.com>
2020-09-04 13:32:42 +0430

    modal

--- frontend/templates/melorin2/assets/js/layouts/modal.js
+++ frontend/templates/melorin2/assets/js/layouts/modal.js
@@ -3,23 +3,35 @@ $(document).ready(function () {
     //register modal
 
     $('.sign_btn').click(function () {
+        $('body').css('overflow','hidden');
         $('#modal_register').fadeIn();
         $('#modal_login').fadeOut();
+
+
     });
 
     $('.btn_close').click(function () {
+        $('body').css('overflow','auto');
         $('#modal_register').fadeOut();
         $('#modal_login').fadeOut();
+        $('#modal_recovery').fadeOut();
     });
 
     $('.register_link').click(function () {
         $('#modal_register').fadeOut();
         $('#modal_login').fadeIn();
+        $('#modal_recovery').fadeOut();
     });
 
     $('.login_link').click(function () {
         $('#modal_register').fadeIn();
         $('#modal_login').fadeOut();
+
     });
+
+    $('.recovery_link').click(function () {
+        $('#modal_recovery').fadeIn();
+        $('#modal_login').fadeOut();
+    })
 });
 
