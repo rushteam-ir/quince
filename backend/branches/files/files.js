c3a9e422fb944a63adc46319ab102d056f947693
rouzbehsbz <polybius1999@gmail.com>
2020-09-09 14:32:07 +0430

    add rushteam logo

--- backend/branches/files/files.js
+++ backend/branches/files/files.js
@@ -4,14 +4,37 @@ router.get('/', async(req, res, next)=>{
 
     try{
 
-        let data = {
+        await serverHelpers.tableList(req, async (page_number, page_limit, data)=>{
 
-            content_list : await fileManager.getPathContent(backend_upload_dir, backend_upload_dir),
-            disk_info : await fileManager.getDiskInfo(backend_upload_dir),
+            let search_inp = req.query.search;
+            let path_inp = req.query.path;
+            let search_pagination = 
 
-        }
+            if(!isUndefined(search_inp)) {
+                data.search = true;
+                data.search_value = search_inp;
+            }
 
-        res.render('files/files', data);
+            data.pagination_url = `/?search=${search_inp}`;
+
+            let category_list = await category_model.search(query, search_inp ? search_inp : null, page_number, page_limit);
+
+            if(category_list.list.length == 0 && category_list.total_pages != 0){
+
+                return res.redirect(`${config.backend_url}category/?page=${category_list.total_pages}`)
+
+            }
+
+            data.parent_list = await category_model.getParent();
+            data.category_list = category_list.list;
+            data.page_number = page_number;
+            data.page_limit = page_limit;
+            data.rows_begin_number = category_list.rows_begin_number;
+            data.total_pages = category_list.total_pages;
+
+            res.render('files/files', data);
+
+        })
 
     }
     catch (error) {
