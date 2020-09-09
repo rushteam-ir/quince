866e191db6a6b24e606eb88f4231ac7456365490
rouzbehsbz <polybius1999@gmail.com>
2020-09-09 16:53:59 +0430

    add delete to file manager api

--- backend/templates/quince/assets/js/files/files.js
+++ backend/templates/quince/assets/js/files/files.js
@@ -25,13 +25,13 @@ $('.download_btn').click(function (){
 
     redirect(`${backend_url}api/file-manager/download/?path=${path}`)
 
-})
+});
 
 $('.table_home_btn').click(function (){
 
     redirect(`${backend_url}files`)
 
-})
+});
 
 $('.table_backward_btn').click(function (){
 
@@ -39,8 +39,37 @@ $('.table_backward_btn').click(function (){
 
     redirect(`${backend_url}files/?path=${path}`)
 
+});
+
+$('.file_delete').on('click', function (e) {
+
+    let path = $(e.currentTarget).attr('name');
+
+    Swal.fire({
+        title: 'حذف فایل',
+        text: "آیا از حذف این فایل مطمئن هستید ؟",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'بله',
+        cancelButtonText: 'خیر',
+    }).then((result) => {
+        if (result.value) {
+            Swal.fire({
+                title: 'حذف شد',
+                text: 'فایل مور نظر با موفقیت حذف شد برای تایید نهایی کلیک کنید',
+                icon: 'success',
+                confirmButtonText: 'تایید',
+            }).then((result) => {
+                if (result.value) {
+                    redirect(`${backend_url}api/file-manager/delete/?path=${path}`);
+                }
+            })
+        }
+    })
 })
 
 function redirect(url) {
     location.href = url
-}
+};
